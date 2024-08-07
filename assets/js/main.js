8727e72262dab15ec341ff9ba865bf206a92e99e
guisfons <guilhermesfonsecaa@gmail.com>
2024-08-07 16:10:12 -0300

    Fixing css issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,11 @@
 window.addEventListener('load', function() {
+    // document.querySelectorAll('a[href^="#contact"]').forEach(function(link) {
+    //     link.addEventListener('click', function(event) {
+    //         event.preventDefault()
+    //         document.querySelector('#contact').scrollIntoView()
+    //     })
+    // })
+
     gsap.registerPlugin(ScrollTrigger)
 
     const sections = document.querySelectorAll('main section')
